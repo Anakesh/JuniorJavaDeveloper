cd34a1dfa7404df28a7b9fcabac62d5abc223797
Anakesh <pavel.frolov.gen@gmail.com>
2018-10-24 19:58:03 +0300

    war and peace

--- src/homework_3_24_10/task4_24_10.java
+++ src/homework_3_24_10/task4_24_10.java
@@ -8,12 +8,22 @@ public class task4_24_10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите длинну массива");
-        int length = in.nextInt();
+        int length;
+        do{
+            length = in.nextInt();
+        }
+        while(length%2!=0);
         int[] arr = new int[length];
         Random random = new Random();
+        int leftSum =0 ;
+        int rightSum = 0;
         for(int i=0;i<arr.length;i++){
             arr[i] = random.nextInt(11)-5;
+            if(i+1<=arr.length/2) leftSum+=arr[i];
+            else rightSum +=arr[i];
         }
         System.out.println(Arrays.toString(arr));
+        System.out.println(leftSum==rightSum ? "Суммы равны.":((leftSum>rightSum)?"Сумма левой части больше.":"Сумма правой части больше."));
+
     }
 }
